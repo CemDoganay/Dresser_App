bf211a7673d1a0ff0d10955742166ac5c6ed3256
jhowe1998 <jhowe1998@gmail.com>
2020-02-01 19:49:28 -0800

    Made the camera take a picture, save it in a private folder, and load the image into the screen (next time make smaller commits).

---
+++ app/src/main/java/com/example/dresser_app/TakePicture.java
@@ -0,0 +1,107 @@
+package com.example.dresser_app;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+public class TakePicture extends AppCompatActivity {
+
+    private Button loadImage;
+
+    static final int REQUEST_TAKE_PHOTO = 1;
+    String currentPhotoPath;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_takepicture);
+
+        loadImage = findViewById(R.id.img_ld_btn);
+
+        dispatchTakePictureIntent();
+
+
+        loadImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                File imgFile = new File(currentPhotoPath);
+                if (imgFile.exists()) {
+                    //TextView msg = findViewById(R.id.text_message);
+                    //msg.setText("Image found at:\n" + currentPhotoPath);
+
+                    setPic();
+                }
+
+                else {
+                    TextView msg = findViewById(R.id.text_message);
+                    msg.setText("Image could not be located.");
+                }
+            }
+        });
+    }
+
+    private File createImageFile() throws IOException{
+        //Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileName, ".jpg", storageDir);
+
+        currentPhotoPath = image.getAbsolutePath();
+
+        return image;
+    }
+
+    private void dispatchTakePictureIntent(){
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        //Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            //Create the File where the photo should go
+            File photoFile = null;
+
+            try {
+                photoFile = createImageFile();
+            }
+
+            catch (IOException ex) {
+                //Error occurred while creating the File
+                Log.e("TakePicture", "dispatchTakePictureIntent() - photoFile error: " + ex);
+            }
+
+            //Continue only if the file was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this, "com.example.android.fileprovider", photoFile);
+
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+    private void setPic() {
+        ImageView photoView = findViewById(R.id.new_photo);
+
+        Bitmap bitmap = BitmapFactory.decodeFile(currentPhotoPath);
+        photoView.setImageBitmap(bitmap);
+    }
+}

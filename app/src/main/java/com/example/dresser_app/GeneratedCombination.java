23b275ad528986bf9f99584e6ad364e765507730
Brian Burciaga <awsomus1@live.com>
2020-02-17 15:22:42 -0800

    Algorithm to match colours was created. Has yet to be tested on the app. Made an output function that needs to be elaborated on.

--- app/src/main/java/com/example/dresser_app/GeneratedCombination.java
+++ app/src/main/java/com/example/dresser_app/GeneratedCombination.java
@@ -5,6 +5,42 @@ import android.os.Bundle;
 
 public class GeneratedCombination extends AppCompatActivity {
 
+    void match(int topColour, String bottomColour){
+        String[][] topBottomMatch = {
+                {"red","blue","white","black","grey"},
+                {"red","blue","green","white","black","grey","brown"},
+                {"blue","green","white","black","grey","brown"},
+                {"blue","orange","white","black"},
+                {"purple","black","white","grey"},
+                {"red","blue","green","white","black","grey","brown"},
+                {"red","blue","green","white","black","grey","brown"},
+                {"blue","white","black","grey"}
+        };
+
+        int flag = 0;
+
+        for(int i = 0;;i++){
+            try{
+                if(topBottomMatch[topColour][i] == bottomColour){
+                    flag = 1;
+                    break;
+                }
+            }catch(Exception e){
+                break;
+            }
+        }
+
+        matchedOutput(flag);
+    }
+
+    void matchedOutput(int flag){
+        if(flag == 1){
+            //output clothing somehow
+        }
+        else{
+            //output that no clothing were matched
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

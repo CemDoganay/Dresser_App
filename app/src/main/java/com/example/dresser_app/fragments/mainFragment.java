e4840918dc79a85be13c19100a8f22a22ff2898b
Brian Burciaga <awsomus1@live.com>
2020-03-09 12:53:55 -0700

    Concrete slider activity made hopefully?

---
+++ app/src/main/java/com/example/dresser_app/fragments/mainFragment.java
@@ -0,0 +1,20 @@
+package com.example.dresser_app.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.dresser_app.R;
+
+public class mainFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(
+                R.layout.activity_main, container, false);
+
+        return rootView;
+    }
+}

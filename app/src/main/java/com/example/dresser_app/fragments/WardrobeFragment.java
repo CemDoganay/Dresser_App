e7a8d4314df7af2890c40d479d7c16c292975fe4
Brian Burciaga <awsomus1@live.com>
2020-03-09 18:15:01 -0700

    Still debugging button features with WardrobeFragment

--- app/src/main/java/com/example/dresser_app/fragments/WardrobeFragment.java
+++ app/src/main/java/com/example/dresser_app/fragments/WardrobeFragment.java
@@ -1,5 +1,6 @@
 package com.example.dresser_app.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,10 +8,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.dresser_app.R;
+import com.example.dresser_app.TakePicture;
 
 public class WardrobeFragment extends Fragment {
+
+    private Button mOpen, mAdd, mRemove;
 
     @Nullable
     @Override
@@ -18,6 +23,15 @@ public class WardrobeFragment extends Fragment {
         ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.activity_wardrobe,
                 container,
                 false);
+
+        mAdd = (Button) rootView.findViewById(R.id.add_button);
+        mAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), TakePicture.class);
+                startActivity(intent);
+            }
+        });
 
         return rootView;
     }

139822114d7a843d7cb5557f4a00d3c99b6db9ea
Brian Burciaga <awsomus1@live.com>
2020-03-30 21:46:24 -0700

    Fixed FragmentTest code issue hopefully

--- app/src/androidTest/java/com/example/dresser_app/FragmentTest.java
+++ app/src/androidTest/java/com/example/dresser_app/FragmentTest.java
@@ -2,17 +2,20 @@ package com.example.dresser_app;
 
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
 
 import com.example.dresser_app.fragments.DressMeFragment;
 
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
+@RunWith(AndroidJUnit4.class)
 public class FragmentTest {
     @Rule
     public ActivityTestRule mActivityTestRule = new ActivityTestRule(DressMeFragment.class);
@@ -21,5 +24,10 @@ public class FragmentTest {
     public void checkToolbarTucked() {
         ViewInteraction mainTextView = onView(withId(R.id.toolbar_tucked));
         mainTextView.check(matches(withText("Tucked")));
+    }
+
+    @Test
+    public void testDressMe() {
+
     }
 }

fe094b0af266efa5f332ab25650f568aa025da00
Brian Burciaga <awsomus1@live.com>
2020-03-31 00:25:17 -0700

    checkWardrobeAddText updated

--- app/src/androidTest/java/com/example/dresser_app/FragmentTest.java
+++ app/src/androidTest/java/com/example/dresser_app/FragmentTest.java
@@ -1,37 +1,35 @@
 package com.example.dresser_app;
 
-import android.support.test.espresso.ViewAction;
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.swipeLeft;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
 public class FragmentTest {
+
+
     @Rule
     public ActivityTestRule mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
-/*
-    @Test
-    public void checkToolbarTucked() {
-        ViewInteraction mainTextView = onView(withId(R.id.toolbar_tucked));
-        mainTextView.check(matches(withText("Tucked")));
+
+    @Before
+    public void swiping() {
+        mActivityTestRule.getActivity()
+                .getFragmentManager().beginTransaction();
     }
-*/
     @Test
     public void checkWardrobeAddText(){
         ViewInteraction addButton = onView(withId(R.id.button_add));
-        addButton
-                .perform(swipeLeft(), (ViewAction) withId(R.layout.activity_main))
-                .check(matches(withText("Add")));
+        addButton.check(matches(withText("Add")));
     }
     @Test
     public void checkDressMeText() {

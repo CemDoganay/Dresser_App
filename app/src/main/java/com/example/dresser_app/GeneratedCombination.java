6976cf08c4cb77810d7fb48a88ca32d6d8f6494f
Brian Burciaga <awsomus1@live.com>
2020-03-30 21:09:46 -0700

    I'm trying something to resolve error not using fragment classes.

--- app/src/main/java/com/example/dresser_app/GeneratedCombination.java
+++ app/src/main/java/com/example/dresser_app/GeneratedCombination.java
@@ -2,15 +2,12 @@ package com.example.dresser_app;
 
 import android.content.Intent;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
-import android.nfc.Tag;
+import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -19,13 +16,8 @@ import android.widget.Toast;
 
 import com.example.dresser_app.fragments.DressMeFragment;
 
-import com.example.dresser_app.fragments.DressMeFragment;
-
-import org.w3c.dom.Text;
-
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 
 
 public class GeneratedCombination extends AppCompatActivity {
